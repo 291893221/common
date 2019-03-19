52160c9c1ffef14a5938b2c7f739dd525ac3b548
zengly <291893221@qq.com>
2019-03-19 09:29:32 +0800

    springboot task

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.demo.dao"})
+@EnableScheduling
 public class DemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/demo/task/SpringBootTask.java
@@ -0,0 +1,22 @@
+package com.demo.task;
+
+import com.demo.controller.RoleController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class SpringBootTask {
+    private Logger logger = LoggerFactory.getLogger(RoleController.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.mm.dd HH:mm:ss");
+
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void test() {
+        logger.info("SpringBootTask 定时任务 {}", dateFormat.format(new Date()));
+    }
+}
