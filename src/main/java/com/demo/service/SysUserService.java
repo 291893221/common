86c776f2b8c37c06fb78572c8c76f324a92d2a92
zengly <291893221@qq.com>
2019-03-26 17:27:15 +0800

    service调整

--- src/main/java/com/demo/service/SysUserService.java
+++ src/main/java/com/demo/service/SysUserService.java
@@ -1,17 +1,46 @@
 package com.demo.service;
 
+import com.demo.dao.SysUserMapper;
 import com.demo.model.SysUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
-public interface SysUserService {
-    int deleteByPrimaryKey(Integer id);
+import javax.annotation.Resource;
 
-    int insert(SysUser record);
+@Slf4j
+@Service
+public class SysUserService {
 
-    int insertSelective(SysUser record);
+    @Resource
+    private SysUserMapper sysUserMapper;
 
-    SysUser selectByPrimaryKey(Integer id);
 
-    int updateByPrimaryKeySelective(SysUser record);
+    public int deleteByPrimaryKey(Integer id) {
+        return 0;
+    }
 
-    int updateByPrimaryKey(SysUser record);
+
+    public int insert(SysUser record) {
+        return sysUserMapper.insert(record);
+    }
+
+
+    public int insertSelective(SysUser record) {
+        return 0;
+    }
+
+
+    public SysUser selectByPrimaryKey(Integer id) {
+        return null;
+    }
+
+
+    public int updateByPrimaryKeySelective(SysUser record) {
+        return 0;
+    }
+
+
+    public int updateByPrimaryKey(SysUser record) {
+        return 0;
+    }
 }
