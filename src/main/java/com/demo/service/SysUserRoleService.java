53ed638b2c362eb6c8690f5282c47d08aae03efe
zengly <291893221@qq.com>
2019-03-27 09:05:04 +0800

    字段整理

--- src/main/java/com/demo/service/SysUserRoleService.java
+++ src/main/java/com/demo/service/SysUserRoleService.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class SysUserRoleService {
     @Resource
     private SysUserRoleMapper sysUserRoleMapper;
+
     public int insert(SysUserRole record){
         return sysUserRoleMapper.insert(record);
     }
