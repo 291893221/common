95c2ebcff0f612a8ba3fe305888b4b6511348031
zengly <291893221@qq.com>
2019-03-29 08:28:28 +0800

    SysPermissionService 继承

--- src/main/java/com/demo/service/SysPermissionService.java
+++ src/main/java/com/demo/service/SysPermissionService.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @Slf4j
 @Service
@@ -14,11 +13,4 @@ public class SysPermissionService extends BaseService<SysPermission>{
 	@Resource
 	private SysPermissionMapper sysPermissionMapper;
 
-	//public int insert(SysPermission record) {
-	//	return sysPermissionMapper.insert(record);
-	//}
-
-	public List<SysPermission> list(SysPermission record){
-		return sysPermissionMapper.list(record);
-	}
 }
