7cd1a32ce9bd060501c047f11ffa4f6e1fed65ec
zengly <291893221@qq.com>
2019-03-29 08:27:01 +0800

    SysPermissionMapper 抽取

--- src/main/java/com/demo/dao/SysPermissionMapper.java
+++ src/main/java/com/demo/dao/SysPermissionMapper.java
@@ -3,13 +3,10 @@ package com.demo.dao;
 import com.demo.model.SysPermission;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-
 /**
 * Created by Mybatis Generator 2019/03/28
 */
 @Mapper
 public interface SysPermissionMapper extends BaseMapper<SysPermission>{
 
-	List<SysPermission> list(SysPermission sysPermission);
 }
