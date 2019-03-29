c61a1f00c6c5298104b44a776f1f0a1729feab51
zengly <291893221@qq.com>
2019-03-29 11:33:45 +0800

    permissions字段整合到权限类

--- src/main/java/com/demo/model/SysPermission.java
+++ src/main/java/com/demo/model/SysPermission.java
@@ -2,6 +2,9 @@ package com.demo.model;
 
 import lombok.Data;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
 * Created by Mybatis Generator 2019/03/28
 */
@@ -14,4 +17,15 @@ public class SysPermission extends PageEntity {
     private Integer roleId;
 
     private String permission;
+
+    //将 permission 按逗号分割为了 list
+    private List permissions;
+
+    public List getPermissions() {
+        return Arrays.asList(this.permission.trim().split(","));
+    }
+
+    public void setPermissions(List permissions) {
+        this.permissions = permissions;
+    }
 }
