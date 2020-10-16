a4b199d9ac30ac074b7f319913d0a3017696c274
291893221 <291893221@qq.com>
2020-10-16 16:35:30 +0800

    c

--- V1.0/common/common-demo/src/main/java/com/github/ums/UmsApplication.java
+++ V1.0/common/common-demo/src/main/java/com/github/ums/UmsApplication.java
@@ -3,12 +3,14 @@ package com.github.ums;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
 
 @SpringBootApplication
 @MapperScan("com.github.ums.mapper")
 @EnableSwagger2Doc
+@ComponentScan(basePackages = {"com.github.common","com.github.ums"})
 public class UmsApplication {
 
 	public static void main(String[] args) {
--- V1.0/common/common-web/src/main/java/com/github/common/exception/GlobalExceptionHandler.java
+++ V1.0/common/common-web/src/main/java/com/github/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.github.common.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
-@Component
 public class GlobalExceptionHandler {
 	/**
 	 * 通用异常
@@ -21,16 +19,15 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler
 	public ResultVo exception(Exception e) {
 		log.error("通用异常: {} ", e.getLocalizedMessage());
-//		return ResultVoUtil.error(ResultEnum.UNKNOWN_EXCEPTION);
 		return new ResultVo();
 	}
+	
 	/**
 	 * 自定义异常
 	 */
 	@ExceptionHandler(value = CustomException.class)
 	public ResultVo processException(CustomException e) {
 		log.error("自定义异常: {} -> 错误信息: {}", e.getMethod(), e.getLocalizedMessage());
-//		return ResultVoUtil.error(Objects.requireNonNull(ResultEnum.getByCode(e.getCode())));
 		return new ResultVo();
 	}
 
