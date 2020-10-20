d94d1cc796973a1ad12257b0aab3f70868e8ef26
291893221 <291893221@qq.com>
2020-10-20 15:57:16 +0800

    c

--- V1.0/common/common-demo/src/main/java/com/github/ums/controller/TestController.java
+++ V1.0/common/common-demo/src/main/java/com/github/ums/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.github.ums.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.ums.entity.UmsUser;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +17,22 @@ public class TestController {
 	@ApiOperation(value = "测试", notes = "测试")
 	public Object insert(String a) {
 		log.info("a : {}", a);
-		return a;
+		UmsUser umsUser = new UmsUser();
+		umsUser.setId(1l);
+		umsUser.setDescribe(a);
+		umsUser.setUsername("hello");
+		
+		ArrayList<UmsUser> arrayList = new ArrayList<UmsUser>();
+		arrayList.add(umsUser);
+		arrayList.add(umsUser);
+		arrayList.add(umsUser);
+		arrayList.add(umsUser);
+		arrayList.add(umsUser);
+		arrayList.add(umsUser);
+		arrayList.add(umsUser);
+		arrayList.add(umsUser);
+		arrayList.add(umsUser);
+		arrayList.add(umsUser);
+		return arrayList;
 	}
 }
