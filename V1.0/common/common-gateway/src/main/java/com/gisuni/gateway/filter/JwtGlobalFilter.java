b714e622d74c49e7688b3949b25d7d71ddbb3754
291893221 <291893221@qq.com>
2020-10-22 15:42:21 +0800

    c

---
+++ V1.0/common/common-gateway/src/main/java/com/gisuni/gateway/filter/JwtGlobalFilter.java
@@ -0,0 +1,42 @@
+package com.gisuni.gateway.filter;
+
+import java.net.URI;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class JwtGlobalFilter implements GlobalFilter, Ordered {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		ServerHttpRequest request = exchange.getRequest();
+		URI uri = request.getURI();
+		log.info("========== uri:" + uri);
+		MultiValueMap<String, String> queryParams = request.getQueryParams();
+		String token = queryParams.getFirst("token");
+		log.info("========== token:" + token);
+		if (token == null || token.isEmpty()) {
+			exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+			return exchange.getResponse().setComplete();
+		}
+		log.info("========== jwt");
+		return chain.filter(exchange);
+	}
+
+	@Override
+	public int getOrder() {
+		return 1;
+	}
+
+}
