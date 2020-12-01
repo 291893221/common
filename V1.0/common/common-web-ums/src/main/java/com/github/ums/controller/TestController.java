185da6ef67df45945d4319534ae1a7f00f97e889
291893221 <291893221@qq.com>
2020-12-01 16:18:44 +0800

    c

--- V1.0/common/common-web-ums/src/main/java/com/github/ums/controller/TestController.java
+++ V1.0/common/common-web-ums/src/main/java/com/github/ums/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.github.ums.controller;
 
+import java.util.Arrays;
+
+import org.junit.Test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.common.annotation.NoRepeatSubmit;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -19,4 +23,16 @@ public class TestController {
 		log.info("entity {}", a);
 		return a;
 	}
+	
+	@Test
+	public void junit() {
+		User a = new User();
+		a.setId("1");
+		var list = Arrays.asList(a);
+		list.stream().peek(e -> log.info("a : {}", e));
+	}
+}
+@Data
+class User {
+	String id;
 }
